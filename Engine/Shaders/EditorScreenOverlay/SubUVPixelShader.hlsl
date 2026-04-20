c4def73d2713f0030130f59c8f0936f94858978e
an_yan_yang <yanggogi1423@gmail.com>
2026-04-20 10:08:43 +0900

    Add : Models

--- Engine/Shaders/EditorScreenOverlay/SubUVPixelShader.hlsl
+++ Engine/Shaders/EditorScreenOverlay/SubUVPixelShader.hlsl
@@ -7,6 +7,7 @@ cbuffer SubUVConstantBuffer : register(b2)
 {
 	float2 CellSize;
 	float2 UVOffset;
+	float4 BaseColor;
 };
 
 Texture2D MainTexture : register(t0);
@@ -16,6 +17,7 @@ float4 main(VS_OUTPUT Input) : SV_TARGET
 {
 	float2 FinalUV = Input.UV * CellSize + UVOffset;
 	float4 Sampled = MainTexture.Sample(MainSampler, FinalUV);
+	Sampled *= BaseColor;
 
 	clip(Sampled.a - 0.01f);
 
